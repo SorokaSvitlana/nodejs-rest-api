61b291823ffad682f6ffdbebb6089a1cf3ed0a1a
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-07-24 12:36:41 +0300

    Fix Schema

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,26 @@
 import express from "express";
-import Joi from "joi";
 import contactsService, { contactsSchema } from "../../models/contacts.js"
 import isValidId from "../../middlewars/isValidid.js";
 import isEmptyBody from "../../middlewars/isEmptyBody.js";
 import validateBody from "../../decorators/validateBody.js";
+import { contactsAddSchema, updateFavoriteSchema } from "../../contactSchema/contactSchema.js";
 
-const contactsAddSchema = Joi.object({
-  name: Joi.string().required().messages({
-      "any.required": `"name" must be exist`,
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" must be exist`,
-}),
-phone: Joi.string().required().messages({
-  "any.required": `phone" must be exist`,
-})
-})
+// const contactsAddSchema = Joi.object({
+//   name: Joi.string().required().messages({
+//       "any.required": `"name" must be exist`,
+//   }),
+//   email: Joi.string().required().messages({
+//     "any.required": `"email" must be exist`,
+// }),
+// phone: Joi.string().required().messages({
+//   "any.required": `phone" must be exist`,
+// })
+// })
 
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required().messages({
-    "any.required": `favorite" must be exist`,})
-})
+// const updateFavoriteSchema = Joi.object({
+//   favorite: Joi.boolean().required().messages({
+//     "any.required": `favorite" must be exist`,})
+// })
 
 const router = express.Router()
 
