58a03582303be725c47e725f661d36da55fcd233
SorokaSvitlana <113341436+SorokaSvitlana@users.noreply.github.com>
2023-07-25 08:08:31 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,26 +1,10 @@
 import express from "express";
-import Joi from "joi";
 import contactsService from "../../conrtollers/contacts-controller.js"
 import isValidId from "../../middlewars/isValidid.js";
 import isEmptyBody from "../../middlewars/isEmptyBody.js";
 import validateBody from "../../decorators/validateBody.js";
+import { contactsAddSchema, updateFavoriteSchema } from "../../contactSchema/contactSchema.js";
 
-const contactsAddSchema = Joi.object({
-  name: Joi.string().required().messages({
-      "any.required": `"name" must be exist`,
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" must be exist`,
-}),
-phone: Joi.string().required().messages({
-  "any.required": `phone" must be exist`,
-})
-})
-
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required().messages({
-    "any.required": `favorite" must be exist`,})
-})
 
 const router = express.Router()
 
